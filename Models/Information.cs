8074649ef0b098cfa4a1d4c71f30016138317155
MyFirstWebApp <sunidhi.gupta@nagarro.com>
2021-07-23 02:06:11 +0530

    Intial commit

---
+++ Models/Information.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstWebApp.Models
+{
+    public class Information
+    {
+        public string Name { get; set; }
+        
+        public string Designation { get; set; }
+        public string Company_name { get; set; }
+        public string Domain { get; set; }
+
+    }
+}
